563997b2a02497d55d1826a2585806540e71a1ce
Kevin(Yuxuan) Yang <kevinyang970@hotmail.com>
2018-05-06 06:15:54 +1000

    improve sidebar toggled animation

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,6 +1,22 @@
 function toggleDp() {
+  if (document.getElementById("line-icon").classList.contains("rolling")){
+    document.getElementById("line-icon").classList.remove("rolling");
+    document.getElementById("line-icon").classList.add("rollback");
+  } else {
+    document.getElementById("line-icon").classList.remove("rollback");
+    document.getElementById("line-icon").classList.add("rolling");
+  }
+  document.getElementById("Dropdown").classList.toggle("show");
+
+  /*
+  if (document.getElementById("line-icon").classList.contains("rolling")){
+    document.getElementById("line-icon").classList.remove("rolling");
+    document.getElementById("line-icon").classList.toggle("rollback");
+  }
+  document.getElementById("line-icon").classList.remove("rollback");
   document.getElementById("line-icon").classList.toggle("rolling");
   document.getElementById("Dropdown").classList.toggle("show");
+  */
 }
 
 window.onclick = function(event) {
@@ -10,6 +26,7 @@ window.onclick = function(event) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
+        document.getElementById("line-icon").classList.toggle("rollback");
         document.getElementById("line-icon").classList.remove("rolling")
       }
     }
